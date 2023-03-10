e6a567d08945f18967a3454322ddc008a744330a
Laughable33 <120147612+Laughable33@users.noreply.github.com>
2023-03-10 17:23:48 -0600

    Day One, Basic Mad Libs Game.

---
+++ Day1/BasicMadLibsGame.py
@@ -0,0 +1,12 @@
+yourName = input("Enter your name: ")
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+favoritePerson = input("Enter the name of your favorite person.: ")
+
+
+print("Whats up", yourName, "shawty")
+print("Roses are", color)
+print(pluralNoun + " are blue")
+print("I love", favoritePerson)
+
+# Mad Libs Game kinda. It's a game where you fill in the blanks with words of your choice.
